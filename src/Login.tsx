1fcdf65cbf4bbbbff4bb2169c0c33b05f340db41
Thomas Lucking <telucking@gmail.comm>
2025-03-31 15:37:02 +0200

    Succesfully added firebase

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const Login = ({ navigation }: any) => {
   return (
